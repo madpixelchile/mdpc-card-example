b181a029ca584014b018f6d6cabc9bdc525567e3
Juan Escudero <juanescudero@MacBook-Pro-de-Juan.local>
2023-09-25 10:30:15 -0300

    package with fonts

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,12 +1,20 @@
 const postcss = require('rollup-plugin-postcss');
 const images = require('@rollup/plugin-image');
+const copy = require('rollup-plugin-copy'); // Añadimos el plugin para copiar archivos
+
 module.exports = {
-    rollup(config, options) {
-        config.plugins = [
-            postcss({ modules: true }),
-            images({ incude: ['**/*.png', '**/*.jpg'] }),
-            ...config.plugins,
-        ];
-        return config;
-    },
+  rollup(config, options) {
+    config.plugins = [
+      postcss({ modules: true }),
+      images({ include: ['**/*.png', '**/*.jpg'] }),
+      copy({
+        targets: [
+          { src: 'src/assets/**/*.scss', dest: 'dist/assets/styles' },
+          { src: ['src/assets/**/*.woff', 'src/assets/**/*.eot', 'src/assets/**/*.svg', 'src/assets/**/*.woff2'], dest: 'dist/assets/fonts' },
+        ],
+      }),
+      ...config.plugins,
+    ];
+    return config;
+  },
 };
